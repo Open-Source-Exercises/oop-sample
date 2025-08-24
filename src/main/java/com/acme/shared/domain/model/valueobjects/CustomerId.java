506704f5c26bc19ba0e2fede71210c761d0479e4
Angel Velasquez <pcisavel@upc.edu.pe>
2025-08-24 08:18:24 -0500

    feat(customer-id): add customer id value object.

---
+++ src/main/java/com/acme/shared/domain/model/valueobjects/CustomerId.java
@@ -0,0 +1,29 @@
+package com.acme.shared.domain.model.valueobjects;
+
+import java.util.Objects;
+import java.util.UUID;
+
+/**
+ * Represents a unique identifier for a customer across the bounded contexts.
+ *
+ * @param value the customer identifier, it must not be null
+ *
+ * @author Open Source Application Development Team
+ */
+public record CustomerId(UUID value) {
+    /**
+     * Constructs a CustomerId object with validation.
+     * @param value the customer identifier, it must not be null
+     */
+    public CustomerId {
+        if (Objects.isNull(value))
+            throw new IllegalArgumentException("Customer identifier cannot be null");
+    }
+
+    /**
+     * Constructs a CustomerId object with a random UUID value.
+     */
+    public CustomerId() {
+        this(UUID.randomUUID());
+    }
+}
