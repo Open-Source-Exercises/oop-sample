fe774e29d9c2e2b0ac893548d431923cda612170
Angel Velasquez <pcisavel@upc.edu.pe>
2025-08-24 20:04:55 -0500

    feat(sales-order-item): add sales order item entity.

---
+++ src/main/java/com/acme/oop/sales/domain/model/aggregates/SalesOrderItem.java
@@ -0,0 +1,41 @@
+package com.acme.oop.sales.domain.model.aggregates;
+
+import com.acme.oop.sales.domain.model.valueobjects.ProductId;
+import com.acme.oop.shared.domain.model.valueobjects.Money;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+
+/**
+ * Represents an item within a Sales Order.
+ * This entity is managed by the Sales Order aggregate.
+ * @author Open Source Application Development Team
+ */
+@Getter
+public class SalesOrderItem {
+    private final ProductId productId;
+    @Setter private int quantity;
+    @Setter private Money unitPrice;
+
+    /**
+     * Constructs a SalesOrderItem instance.
+     * @param productId the product identifier, it must not be null
+     * @param quantity  the quantity of the product, it must be greater than zero
+     * @param unitPrice the unit price of the product, it must not be null and must be greater than zero
+     */
+    SalesOrderItem(@NonNull ProductId productId, int quantity, @NonNull Money unitPrice) {
+        if (quantity <= 0)
+            throw new IllegalArgumentException("Quantity must be greater than zero");
+        this.productId = productId;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+    }
+
+    /**
+     * Calculates the total price of the item.
+     * @return the total price of the item
+     */
+    public Money calculateItemPrice() {
+        return unitPrice.multiply(quantity);
+    }
+}
